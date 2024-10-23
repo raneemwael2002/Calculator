4be306ac7d2025f4f7b5ec7606c4c34018b50d01
raneemwael2002 <ranim195388@feng.bu.edu.eg>
2024-10-23 13:58:21 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std
 
+int main() {
+	int num1, num2, result,choice;
+	cout << "Welcome to the program, choose a number: \n 1-Sum \n 2- Sub \n 3-Mul \n 4- Div \n 5-Power \n 6-Mod ";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1: {
+		break;
+	}
+	case 2: {
+		break;
+	}
+	case 3: {
+		break;
+	}
+	case 4: {
+		break;
+	}
+	case 5: {
+		break;
+	}
+	case 6: {
+		break;
+	}
+	default:
+		cout << "Please enter a valid option."
+		break;
+	}
+
+
+
+	return 0;
+}

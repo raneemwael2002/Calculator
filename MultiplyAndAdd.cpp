68fdf2f77a9ad0143f8add34366a6fc5983d2b09
raneemwael2002 <ranim195388@feng.bu.edu.eg>
2024-10-23 15:21:24 +0300

    adding functions

--- MultiplyAndAdd.cpp
+++
@@ -1,9 +0,0 @@
-public void Multiply(int a, int b)
-{
-	int result = a * b;
-}
-
-public void Divide(int a, int b)
-{
-	int result = a / b;
-}
--- MultiplyAndAdd.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-public void Multiply(int a, int b);
-public void Divide(int a, int b);

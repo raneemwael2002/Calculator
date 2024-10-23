491e00651f8f921b0577406e76b5947e1de9db6e
raneemwael2002 <66146343+raneemwael2002@users.noreply.github.com>
2024-10-23 15:23:04 +0300

    Update Calculator2.cpp

--- Calculator2/Calculator2.cpp
+++ Calculator2/Calculator2.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main() {
 	int num1 = 1, num2 = 2, result = 0, choice;
-	cout << "Welcome to the program, choose a number: \n 1-Sum \n 2- Sub \n 3-Mul \n 4- Div \n 5-Power \n 6-Mod ";
+	cout << "Welcome to the program, choose a number: \n 1-Sum \n 2- Sub \n 3-Mul \n 4- Div \n 5-Power \n 6-Mod \n ";
 	cin >> choice;
-	cout << "Please enter your numbers";
+	cout << "Please enter your numbers: ";
 	cin >>num1 >> num2;
 	
 	switch (choice)

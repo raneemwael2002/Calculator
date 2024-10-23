b4b1aa197f761c7802094f261fd5d48a1f9dca46
Fouad Elamoury <foad.osama@hotmail.com>
2024-10-23 14:16:47 +0300

    adding multiply

---
+++ MultiplyAndAdd.cpp
@@ -0,0 +1,9 @@
+public void Multiply(int a, int b)
+{
+	int result = a * b;
+}
+
+public void Divide(int a, int b)
+{
+	int result = a / b;
+}
---
+++ MultiplyAndAdd.h
@@ -0,0 +1,3 @@
+#pragma once
+public void Multiply(int a, int b);
+public void Divide(int a, int b);
